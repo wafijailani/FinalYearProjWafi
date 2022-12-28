8bcdfb107d64c33d49f7a3ffc460895c63f5e5ef
wafijailani <wafijailani10@live.com>
2022-12-28 15:59:25 +0800

    design

---
+++ resources/views/admin/adminjs.blade.php
@@ -0,0 +1,17 @@
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="adminuser/assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="adminuser/assets/vendors/chart.js/Chart.min.js"></script>
+    <script src="adminuser/assets/js/jquery.cookie.js" type="text/javascript"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="adminuser/assets/js/off-canvas.js"></script>
+    <script src="adminuser/assets/js/hoverable-collapse.js"></script>
+    <script src="adminuser/assets/js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="adminuser/assets/js/dashboard.js"></script>
+    <script src="adminuser/assets/js/todolist.js"></script>
+    <!-- End custom js for this page -->
--- resources/views/admin/viewsuspendedusers.blade.php
+++ resources/views/admin/viewsuspendedusers.blade.php
@@ -1,37 +1,129 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+@include("admin.admincss")
 </head>
 <body>
-@if (session('status'))
+
+        <div class="container-scroller">
+      <!-- partial:partials/_navbar.html -->
+      <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+        <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+          <a class="navbar-brand brand-logo" href="index.html"><img src="adminuser/assets/images/logo.svg" alt="logo" /></a>
+          <a class="navbar-brand brand-logo-mini" href="index.html"><img src="adminuser/assets/images/logo-mini.svg" alt="logo" /></a>
+        </div>
+        <div class="navbar-menu-wrapper d-flex align-items-stretch">
+          <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+            <span class="mdi mdi-menu"></span>
+          </button>
+
+          <ul class="navbar-nav navbar-nav-right">
+            <li class="nav-item nav-profile dropdown">
+              <a class="nav-link dropdown-toggle" id="profileDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                <div class="nav-profile-img">
+                  <img src="adminuser/assets/images/faces/face1.jpg" alt="image">
+                  <span class="availability-status online"></span>
+                </div>
+                <div class="nav-profile-text">
+                  <p class="mb-1 text-black">David Greymaax</p>
+                </div>
+              </a>
+              <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
+                <a class="dropdown-item" href="#">
+                  <i class="mdi mdi-cached me-2 text-success"></i> Activity Log </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  <i class="mdi mdi-logout me-2 text-primary"></i> Signout </a>
+              </div>
+            </li>
+            <li class="nav-item nav-logout d-none d-lg-block">
+              <a class="nav-link" href="#">
+                <i class="mdi mdi-power"></i>
+              </a>
+            </li>
+          </ul>
+          <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+            <span class="mdi mdi-menu"></span>
+          </button>
+        </div>
+      </nav>
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        <nav class="sidebar sidebar-offcanvas" id="sidebar">
+          <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link" href="/viewusers">
+                <span class="menu-title">View Users</span>
+                <i class="mdi mdi-contacts menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/viewuserssuspend">
+                <span class="menu-title">View Suspended Users</span>
+                <i class="mdi mdi-format-list-bulleted menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/viewposts">
+                <span class="menu-title">View Posts</span>
+                <i class="mdi mdi-chart-bar menu-icon"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <!-- partial -->
+        <div class="main-panel">
+          <div class="content-wrapper">
+          @if (session('status'))
                 <h6 class="alert alert-success">{{ session('status') }}</h6>
                 @endif
-    <h1>Suspended Users</h1>
-
-    <table class="table">
-                        <thead>
-                          <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Phone Number</th>
-                            <th>District</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                          </tr>
-                        </thead>
-                        <tbody>
+          <div class="row">
+              <div class="col-lg-6 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">View All Current Users</h4>
+                    <table class="table table-striped">
+                      <thead>
                         <tr>
-                @foreach($values as $value)
-                <td>{{$value->name}}</td>
-                <td>{{$value->email}}</td>
-                <td>{{$value->phonenum}}</td>
-                <td>{{$value->district}}</td>
-                <td><a href="{{ url('reactivate/'.$value->id) }}" class="btn btn-danger btn-sm"> Reactivate User</a></td>
-            </tr>
-            @endforeach
+                          <th>Name</th>
+                          <th>Email</th>
+                          <th>Phone Number</th>
+                          <th>District</th>
+                          <th></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          @foreach($values as $value)
+                          <td>{{$value->name}}</td>
+                          <td>{{$value->email}}</td>
+                          <td>{{$value->phonenum}}</td>
+                          <td>{{$value->district}}</td>
+                          <td><a href="{{ url('reactivate/'.$value->id) }}" class="btn btn-danger btn-sm"> Reactivate User</a></td>
+                        </tr>
+                          @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+</div>
+          </div>
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+          <footer class="footer">
+            <div class="container-fluid d-flex justify-content-between">
+              <span class="text-muted d-block text-center text-sm-start d-sm-inline-block">Developed by Abdul Wafi Bin Jailani @ Universiti Tenaga Nasional</span>
+            </div>
+          </footer>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+
+@include("admin.adminjs")
 </body>
 </html>
